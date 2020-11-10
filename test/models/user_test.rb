9e54f79b3c30fef4685790fd5a6ddc1bd5554054
MichalChojnacki450 <58258243+MichalChojnacki450@users.noreply.github.com>
2020-11-10 13:16:48 +0100

    test+gems

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,31 +5,26 @@ class UserTest < ActiveSupport::TestCase
   #   assert true
   # end
 end
-
 describe "User visits theri show page" do
   context "if account has not been authenticated" do
       it "user authorizes account", :driver => :mechanize do
-          user = User.create!(first_name: "michal",
-                              last_name: "chojnacki",
-                              email: "michal@gmail.com",
+          user = User.create!(email: "michal@gmail.com",
                               password: '12345')
 
           allow_any_instance_off(ApplicationController).to recive(:current_user).and_return(user)
 
-          visit "/users/#{user.id}"
+          visit "/home/#{user.id}"
 
-          expect(page).to have_link("Add to Account")
+          expect(page).to have_link("sign In")
+          click_button "sign In"
+
           expect(User.find(user.id).athlete_id).to be_nil
           expect(User.find(user.id).access_token).to be_nil
-
-          click_on "Add Account"
 
           fill_in :email, with: ENV[TEST_ENVIRONMENT_EMAIL]
           fill_in :password, with: ENV[TEST_ENVIRONMENT_PASSWORD]
 
           click_button "Log In"
-
-          click_button "Authorize"
 
           expect(current_path).to eq(user_path(user))
           expect(User.find(user.id).athlete_id).to be_truthy
@@ -37,3 +32,4 @@ describe "User visits theri show page" do
       end
   end
 end
+
